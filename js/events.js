b49c9fad8bb843e4917228d51de5006975d559b4
Nick Bradley <nick.bradley.193@pm.me>
2018-04-25 22:20:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,20 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  
 });
